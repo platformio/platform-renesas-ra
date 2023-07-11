2a877c449f588c40425badb1cf1343a64e8f8834
maxgerhardt <maximilian.gerhardt@rub.de>
2023-07-11 19:19:28 +0200

    Add back popular examples

---
+++ examples/arduino-wifiscan/src/main.cpp
@@ -0,0 +1,108 @@
+#include <Arduino.h>
+#if defined(ARDUINO_UNOR4_WIFI)
+#include <WiFiS3.h>
+#elif defined(ARDUINO_PORTENTA_C33)
+#include <WiFiC3.h>
+#else
+#error "Unsupported board for this sketch!"
+#endif
+
+void printEncryptionType(int thisType) {
+  // read the encryption type and print out the name:
+  switch (thisType) {
+    case ENC_TYPE_WEP:
+      Serial.println("WEP");
+      break;
+    case ENC_TYPE_WPA:
+      Serial.println("WPA");
+      break;
+    case ENC_TYPE_WPA2:
+      Serial.println("WPA2");
+      break;
+    case ENC_TYPE_WPA3:
+      Serial.print("WPA3");
+      break;   
+    case ENC_TYPE_NONE:
+      Serial.println("None");
+      break;
+    case ENC_TYPE_AUTO:
+      Serial.println("Auto");
+      break;
+    case ENC_TYPE_UNKNOWN:
+    default:
+      Serial.println("Unknown");
+      break;
+  }
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+}
+
+
+void listNetworks() {
+  // scan for nearby networks:
+  Serial.println("** Scan Networks **");
+  int numSsid = WiFi.scanNetworks();
+  if (numSsid == -1) {
+    Serial.println("Couldn't get a WiFi connection");
+    while (true);
+  }
+
+  // print the list of networks seen:
+  Serial.print("number of available networks:");
+  Serial.println(numSsid);
+
+  // print the network number and name for each network found:
+  for (int thisNet = 0; thisNet < numSsid; thisNet++) {
+    Serial.print(thisNet);
+    Serial.print(") ");
+    Serial.print(WiFi.SSID(thisNet));
+    Serial.print(" Signal: ");
+    Serial.print(WiFi.RSSI(thisNet));
+    Serial.print(" dBm");
+    Serial.print(" Encryption: ");
+    printEncryptionType(WiFi.encryptionType(thisNet));
+  }
+}
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+}
+
+void loop() {
+  byte mac[6];
+  // scan for existing networks:
+  Serial.println("Scanning available networks...");
+  listNetworks();
+  WiFi.macAddress(mac);
+  Serial.println();
+  Serial.print("Your MAC Address is: ");
+  printMacAddress(mac);
+  delay(10000);
+}
