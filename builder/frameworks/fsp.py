04be98f66d9bdadacce00786522c4f37e7ebffbc
maxgerhardt <maximilian.gerhardt@rub.de>
2023-07-12 12:16:51 +0200

    Fix extra spaces

--- builder/frameworks/fsp.py
+++ builder/frameworks/fsp.py
@@ -52,17 +52,17 @@ env.Append(
 libs = []
 
 # Extracted from ArduinoCore-Renesas\extras\e2studioProjects\Santiago\configuration.xml "raComponentSelection"
-sublibs_uno_r4_x = ["r_adc", "r_agt", "r_cgc", "r_dac", "r_doc", "r_dmac", "r_dtc", 
+sublibs_uno_r4_x = ["r_adc", "r_agt", "r_cgc", "r_dac", "r_doc", "r_dmac", "r_dtc",
                     "r_elc", "r_gpt", "r_icu", "r_iic_master", "r_iic_slave", "r_ioport",
-                    "r_kint", "r_lpm", "r_rtc", "r_sce/crypto_procedures/src/sce5", "r_sce5_ra4", 
-                    "r_sci_i2c", "r_sci_spi", "r_sci_uart", "r_spi", "r_usb_basic", "r_usb_pcdc", 
+                    "r_kint", "r_lpm", "r_rtc", "r_sce/crypto_procedures/src/sce5", "r_sce5_ra4",
+                    "r_sci_i2c", "r_sci_spi", "r_sci_uart", "r_spi", "r_usb_basic", "r_usb_pcdc",
                     "r_wdt", "r_can", "r_flash_lp", "rm_vee_flash"]
 # Extracted from ArduinoCore-Renesas\extras\e2studioProjects\portenta_h33_lib
 sublibs_portenta_c33 = ["r_adc", "r_agt", "r_canfd", "r_cgc", "r_crc", "r_dac", "r_dmac", "r_dtc",
                         "r_elc", "r_ether_phy", "r_ether", "r_flash_hp", "r_gpt", "r_icu", "r_iic_master",
-                        "r_iic_slave", "r_ioport", "r_lpm", "r_qspi", "r_rtc", 
-                        "r_sce_protected/crypto_procedures/src/sce9", "r_sci_i2c", "r_sci_spi", 
-                        "r_sci_uart", "r_spi", "r_ssi", "r_usb_basic", "r_usb_pcdc", "r_usb_composite", 
+                        "r_iic_slave", "r_ioport", "r_lpm", "r_qspi", "r_rtc",
+                        "r_sce_protected/crypto_procedures/src/sce9", "r_sci_i2c", "r_sci_spi",
+                        "r_sci_uart", "r_spi", "r_ssi", "r_usb_basic", "r_usb_pcdc", "r_usb_composite",
                         "r_wdt", "rm_adpcm_decoder", "r_sdhi"]
 # This is always built
 src_filter = [
