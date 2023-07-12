170b200dc9b2bebbeb37b29fad9b4bcb1abe94bd
maxgerhardt <maximilian.gerhardt@rub.de>
2023-07-12 12:11:20 +0200

    Fix EOL, fix code formatting, extract variables, src_filter as list

--- builder/frameworks/fsp.py
+++ builder/frameworks/fsp.py
@@ -5,6 +5,7 @@ from SCons.Script import DefaultEnvironment
 env = DefaultEnvironment()
 platform = env.PioPlatform()
 board = env.BoardConfig()
+variant = variant
 
 env.SConscript("_bare.py")
 
@@ -33,37 +34,48 @@ env.Append(
         join(FRAMEWORK_DIR, "fsp", "src", "r_sce"),
         join(FRAMEWORK_DIR, "fsp", "src", "r_sce", "common"),
         join(FRAMEWORK_DIR, "fsp", "inc", "arm", "CMSIS_5", "CMSIS", "Core", "Include"),
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant")),
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant"), "includes", "ra_gen"),
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant"), "includes", "ra_cfg", "fsp_cfg"),
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant"), "includes", "ra_cfg", "fsp_cfg", "bsp"),
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant"), "tmp_gen_c_files")
+        join(FRAMEWORK_DIR, "variants", variant),
+        join(FRAMEWORK_DIR, "variants", variant, "includes", "ra_gen"),
+        join(FRAMEWORK_DIR, "variants", variant, "includes", "ra_cfg", "fsp_cfg"),
+        join(FRAMEWORK_DIR, "variants", variant, "includes", "ra_cfg", "fsp_cfg", "bsp"),
+        join(FRAMEWORK_DIR, "variants", variant, "tmp_gen_c_files")
     ],
     LINKFLAGS=[
         "--specs=nano.specs"
     ],
     LIBPATH=[
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant"))
+        join(FRAMEWORK_DIR, "variants", variant)
     ],
-    LDSCRIPT_PATH=join(FRAMEWORK_DIR, "variants", board.get("build.variant"), "fsp.ld")
+    LDSCRIPT_PATH=join(FRAMEWORK_DIR, "variants", variant, "fsp.ld")
 )
 
 libs = []
 
 # Extracted from ArduinoCore-Renesas\extras\e2studioProjects\Santiago\configuration.xml "raComponentSelection"
-to_be_built_sublibs_uno_r4_x = ["r_adc", "r_agt", "r_cgc", "r_dac", "r_doc", "r_dmac", "r_dtc", "r_elc", "r_gpt", "r_icu", "r_iic_master", "r_iic_slave", "r_ioport", "r_kint",
-                       "r_lpm", "r_rtc", "r_sce/crypto_procedures/src/sce5", "r_sce5_ra4", "r_sci_i2c", "r_sci_spi", "r_sci_uart", "r_spi", "r_usb_basic", "r_usb_pcdc", "r_wdt", "r_can", "r_flash_lp", "rm_vee_flash"]
+sublibs_uno_r4_x = ["r_adc", "r_agt", "r_cgc", "r_dac", "r_doc", "r_dmac", "r_dtc", 
+                    "r_elc", "r_gpt", "r_icu", "r_iic_master", "r_iic_slave", "r_ioport",
+                    "r_kint", "r_lpm", "r_rtc", "r_sce/crypto_procedures/src/sce5", "r_sce5_ra4", 
+                    "r_sci_i2c", "r_sci_spi", "r_sci_uart", "r_spi", "r_usb_basic", "r_usb_pcdc", 
+                    "r_wdt", "r_can", "r_flash_lp", "rm_vee_flash"]
 # Extracted from ArduinoCore-Renesas\extras\e2studioProjects\portenta_h33_lib
-to_be_built_sublibs_portenta_c33 = ["r_adc", "r_agt", "r_canfd", "r_cgc", "r_crc", "r_dac", "r_dmac", "r_dtc", "r_elc", "r_ether_phy", "r_ether", "r_flash_hp", "r_gpt", "r_icu", "r_iic_master", "r_iic_slave",
-                                    "r_ioport", "r_lpm", "r_qspi", "r_rtc", "r_sce_protected/crypto_procedures/src/sce9", "r_sci_i2c", "r_sci_spi", "r_sci_uart", "r_spi", "r_ssi", "r_usb_basic", "r_usb_pcdc", "r_usb_composite", "r_wdt", "rm_adpcm_decoder", "r_sdhi"]
+sublibs_portenta_c33 = ["r_adc", "r_agt", "r_canfd", "r_cgc", "r_crc", "r_dac", "r_dmac", "r_dtc",
+                        "r_elc", "r_ether_phy", "r_ether", "r_flash_hp", "r_gpt", "r_icu", "r_iic_master",
+                        "r_iic_slave", "r_ioport", "r_lpm", "r_qspi", "r_rtc", 
+                        "r_sce_protected/crypto_procedures/src/sce9", "r_sci_i2c", "r_sci_spi", 
+                        "r_sci_uart", "r_spi", "r_ssi", "r_usb_basic", "r_usb_pcdc", "r_usb_composite", 
+                        "r_wdt", "rm_adpcm_decoder", "r_sdhi"]
 # This is always built
-src_filter = "-<*> +<bsp/cmsis> +<bsp/mcu/all> "
-if str(board.get("build.mcu", "")).lower().startswith("ra4m1"):
-    src_filter += "+<bsp/mcu/ra4m1> " # Uno R4 (Minima, WiFI)
+src_filter = [
+    "-<*>",
+    "+<bsp/cmsis>",
+    "+<bsp/mcu/all>"
+]
+if board.get("build.mcu", "").lower().startswith("ra4m1"):
+    src_filter.append("+<bsp/mcu/ra4m1>") # Uno R4 (Minima, WiFI)
 else:
-    src_filter += "+<bsp/mcu/ra6m5> " # Portenta
+    src_filter.append("+<bsp/mcu/ra6m5>") # Portenta
 if board.id in ("uno_r4_minima", "uno_r4_wifi"):
-    src_filter += " ".join(["+<" + join(sublib) + ">" for sublib in to_be_built_sublibs_uno_r4_x])
+    src_filter.extend(["+<" + sublib + ">" for sublib in sublibs_uno_r4_x])
     env.Append(CPPPATH=[
         join(FRAMEWORK_DIR, "fsp", "src", "bsp", "mcu", "ra4m1"),
         join(FRAMEWORK_DIR, "fsp", "src", "r_sce", "crypto_procedures", "src", "sce5", "plainkey", "public", "inc"),
@@ -73,7 +85,7 @@ if board.id in ("uno_r4_minima", "uno_r4_wifi"):
         ("_RA_CORE", "CM4")
     ])
 elif board.id == "portenta_c33":
-    src_filter += " ".join(["+<" + join(sublib) + ">" for sublib in to_be_built_sublibs_portenta_c33])
+    src_filter.extend(["+<" + sublib + ">" for sublib in sublibs_portenta_c33])
     env.Append(CPPPATH=[
         join(FRAMEWORK_DIR, "fsp", "src", "bsp", "mcu", "ra6m5"),
         join(FRAMEWORK_DIR, "fsp", "src", "r_sce", "crypto_procedures", "src", "sce9", "plainkey", "public", "inc"),
@@ -82,7 +94,7 @@ elif board.id == "portenta_c33":
         join(FRAMEWORK_DIR, "fsp", "src", "r_sce_protected", "crypto_procedures_protected", "src", "sce9", "inc", "instances"),
         join(FRAMEWORK_DIR, "fsp", "src", "r_sce_protected", "crypto_procedures_protected", "src", "sce9", "private", "inc"),
         join(FRAMEWORK_DIR, "fsp", "src", "r_sce_protected", "crypto_procedures_protected", "src", "sce9", "public", "inc"),
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant"), "includes", "ra_cfg", "driver")
+        join(FRAMEWORK_DIR, "variants", variant, "includes", "ra_cfg", "driver")
     ])
     env.Append(CPPDEFINES=[
         ("_RA_CORE", "CM33")
@@ -97,7 +109,7 @@ libs.append(env.BuildLibrary(
 )
 # Build the glue files
 libs.append(env.BuildLibrary(
-        join("$BUILD_DIR", "FSPFrameworkVariant"),
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant"), "tmp_gen_c_files")))
+    join("$BUILD_DIR", "FSPFrameworkVariant"),
+    join(FRAMEWORK_DIR, "variants", variant, "tmp_gen_c_files")))
 
 env.Prepend(LIBS=libs)
